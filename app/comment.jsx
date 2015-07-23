6b3b6e4029001d959fbadea2f7d6acfbfe102ba3
gtxx <xiongxiong1986@gmail.com>
2015-07-23 16:13:07 +0800

    add comment view

--- app/comment.jsx
+++ app/comment.jsx
@@ -25,29 +25,33 @@ class StoryComments extends React.Component {
   }
 
   render() {
+    function get_length(comments){
+      let len = comments.length;
+      for (let comment of comments){
+        len += get_length(comment.children)
+      }
+      return len;
+    }
     let story = this.state.story;
     let comments = []
-    console.log(story)
+    let comment_total = 0;
     if (story.hasOwnProperty('children')) {
       let children = story.children
-      console.log(children)
+      comment_total = get_length(children);
       children.sort(function(a, b){
         return a.created_at_i < b.created_at_i
       })
-      console.log(children)
       comments = children.map(function (comment, index) {
         return (<Comment comment={comment} key={index} />)
       })
     }
-    console.log(comments)
-    //let comments = []
 
     return (
       <div className='col-sm-12'>
         <Panel
-          header={<h4><a href={story.url}>{story.url}</a> | {story.type}</h4>}>
+          header={<h4><a href={story.url}>{story.title}</a> | {story.type}</h4>}>
           <Badge>{story.points}</Badge> points by <a href={`#/user/${story.author}`}> {story.author}</a> in {moment(story.created_at).fromNow()} |
-          <Badge>{comments.length}</Badge> comments
+          <Badge>{comment_total}</Badge> comments
         </Panel>
         {comments}
       </div>
@@ -60,10 +64,15 @@ class Comment extends React.Component {
   render(){
     let comment = this.props.comment;
     let user_url = `#/user/${comment.author}`;
+    let children = comment.children.map(function(child, index){
+      return (<Comment comment={child} key={index} />)
+    })
+
     return (
       <Panel
         header={<h4><a href={user_url}>{comment.author}</a>  {moment(comment.created_at).fromNow()}</h4>}>
         <div dangerouslySetInnerHTML={{__html: comment.text}} />
+        {children}
       </Panel>
     )
   }
